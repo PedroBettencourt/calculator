2a35a47b131cf027f52caa9ea1b34f38b77ac030
PedroBettencourt <pedropbettencourt@gmail.com>
2025-03-13 17:02:40 +0000

    Create basic structure

---
+++ script.js
@@ -0,0 +1,37 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function multiply(a, b) {
+    return a / b;
+}
+
+let numA;
+let numB;
+let operator;
+
+function operator(a, b, operator) {
+    switch (operator) {
+        case "+":
+            add(a, b);
+            break;
+        case "-":
+            subtract(a, b);
+            break;
+        case "*":
+            multiply(a, b);
+            break;
+        case "/":
+            divide(a, b);
+            break;
+    }
+}
+
