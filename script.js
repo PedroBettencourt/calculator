97582c98285ac9fb782449fa35eff78114a7dfb4
PedroBettencourt <pedropbettencourt@gmail.com>
2025-03-14 14:14:00 +0000

    Add keyboard support

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function operate() {
+function getOperation() {
     a = parseFloat(lastValue);
     b = parseFloat(currValue);
     let result;
@@ -21,14 +21,67 @@ function operate() {
             result = a / b;
             break;
     }
-    currValue = +result.toFixed(8);
-    valueDisplay = currValue;
-    display.textContent = valueDisplay;
-    valueLast = "";
-    valueNow = "";
+    currValue = +result.toFixed(8);  
     currOperation = "";
+    display.textContent = currValue;
 }
 
+function getOperationBefore(operation) {
+    tempOperation = operation;
+
+    if (currValue === "") return
+    if (lastValue !== "" && currOperation !== "") {
+        getOperation()
+    }
+    displayValue = currValue;
+    lastValue = currValue;
+    currValue = "";
+}
+
+function getDisplay(digit) {
+    if(displayValue) {
+        display.textContent = "0";
+        displayValue = "";
+    }
+
+    // Prohibit more than 1 dot
+    if (digit === "." && display.textContent.includes(".")) { return }
+    
+    // Replace 0 so when typing a number 0 doesn't stay as the 1st digit
+    if (display.textContent.at(0) === "0") display.textContent = display.textContent.slice(1);
+    
+    // Store curr operation if operation has been chosen
+    if (tempOperation !== "") {
+        currOperation = tempOperation;
+        tempOperation = "";
+    }
+    currValue += digit;
+    display.textContent += digit;
+}
+
+function getResult() {
+    if (lastValue === "") return;
+
+    getOperation();
+    lastValue = currValue;
+    displayValue = currValue;
+    currValue = "";
+}
+
+function clear() {
+    currValue = "";
+    lastValue = "";
+    currOperation = "";
+    display.textContent = "0";
+}
+
+function deleteValue() {
+    currValue = currValue.slice(0, -1);
+    display.textContent = currValue;
+    if (currValue === "") display.textContent = "0";
+}
+
+// Global variables
 let currValue = "";
 let lastValue = "";
 let currOperation = "";
@@ -37,29 +90,12 @@ let displayValue = "";
 
 // Get digits from buttons
 const display = document.querySelector(".display");
+
 const digit = document.querySelector(".digits");
 digit.addEventListener("click", (e) => {
     const element = e.target
     if (element.classList[0] === "digit") {
-        if(displayValue) {
-            display.textContent = 0;
-            displayValue = "";
-        }
-
-        // Prohibit more than 1 dot
-        if (element.textContent === "." && display.textContent.includes(".")) { return }
-        
-        // Replace 0 so when typing a number 0 doesn't stay as the 1st digit
-        if (display.textContent.at(0) === "0") display.textContent = display.textContent.slice(1);
-        
-        // Store curr operation if operation has been chosen
-        if (tempOperation !== "") {
-            currOperation = tempOperation;
-            tempOperation = "";
-        }
-
-        currValue += element.textContent;
-        display.textContent += element.textContent;
+        getDisplay(element.textContent);
     }
 })
 
@@ -68,41 +104,36 @@ const operationButtons = document.querySelectorAll(".operator");
 operationButtons.forEach((btn) => {
     btn.addEventListener("click", (e) => {
         const operation = e.target.textContent;
-        tempOperation = operation;
-
-        if (currValue === "") return
-        if (lastValue !== "" && currOperation !== "") {
-            operate()
-        }
-        displayValue = currValue;
-        lastValue = currValue;
-        currValue = "";
+        getOperationBefore(operation);        
     })
 });
 
 // Get the result with equal button
 const resultButton = document.querySelector(".result");
-resultButton.addEventListener("click", (e) => {
-    if (lastValue === "") return;
-
-    operate();
-    lastValue = currValue;
-    displayValue = currValue;
-    currValue = "";
+resultButton.addEventListener("click", () => {
+    getResult()
 });
 
 // Clear with the C button
 const clearButton = document.querySelector(".clear");
-clearButton.addEventListener("click", (e) => {
-    currValue = "";
-    lastValue = "";
-    currOperation = "";
-    display.textContent = 0;
+clearButton.addEventListener("click", () => {
+    clear();
 });
 
 // Delete with DEL button
 const deleteButton = document.querySelector(".delete");
 deleteButton.addEventListener("click", () => {
-    currValue = currValue.slice(0, -1);
-    display.textContent = currValue;
+    deleteValue();
+});
+
+// Keyboard support
+document.addEventListener("keydown", (e) => {
+    let key = e.key;
+    if (key === "/") e.preventDefault();
+    if (key === "*") key = "x";
+    if ("0123456789.".includes(key)) getDisplay(key);
+    if ("+-x/".includes(key)) getOperationBefore(key);
+    if (key === "=" || key === "Enter") getResult();
+    if (key === "Backspace" || key === "Delete") deleteValue();
+    if (key === "Escape") clear()
 });
